a60ed0a54daccd789dcc69ad9968846505bd5ddd
unstephenk <unstephenk@gmail.com>
2023-05-25 10:51:11 -0500

    Increasuing Reusability

--- pages/components/houseList.js
+++ pages/components/houseList.js
@@ -1,51 +1,52 @@
 import loadingStatus from "../helpers/loadingStatus";
-import HouseRow from "./houseRow";
 import useHouses from "../hooks/useHouses";
+import HouseRow from "./houseRow";
 import LoadingIndicator from "./loadingIndicator";
 
 const HouseList = ({ selectHouse }) => {
-    const { houses, setHouses, loadingState } = useHouses();
+  const { houses, setHouses, loadingState } = useHouses();
 
-    if (loadingState !== loadingStatus.loaded)
+  if (loadingState !== loadingStatus.loaded)
     return <LoadingIndicator loadingState={loadingState} />;
 
-    const addHouse = () => {
-        setHouses([
-            ...houses,
-            {
-                id: 8,
-                address: "12 Valley of Kings, Geneva",
-                country: "France",
-                price: 700000,
-            },
-        ]);
-    }
+  const addHouse = () => {
+    setHouses([
+      ...houses,
+      {
+        id: 3,
+        address: "32 Valley Way, New York",
+        country: "USA",
+        price: 1000000,
+      },
+    ]);
+  };
 
-    return (
-        <>
-            <div className="row mb-2">
-                <h5 className="themeFrontColor text-center">
-                    Houses currently on the market
-                </h5>
-            </div>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Address</th>
-                        <th>Country</th>
-                        <th>Asking Price</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {houses.map((h) => (<HouseRow key={h.id} house={h} selectHouse={selectHouse} />))}
-                </tbody>
-            </table>
-            <button className="btn btn-primary" onClick={addHouse}>
-                Add
-            </button>
-        </>
-    );
+  return (
+    <>
+      <div className="row mb-2">
+        <h5 className="themeFontColor text-center">
+          Houses currently on the market
+        </h5>
+      </div>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Address</th>
+            <th>Country</th>
+            <th>Asking Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {houses.map((h) => (
+            <HouseRow key={h.id} house={h} selectHouse={selectHouse} />
+          ))}
+        </tbody>
+      </table>
+      <button className="btn btn-primary" onClick={addHouse}>
+        Add
+      </button>
+    </>
+  );
 };
 
 export default HouseList;
